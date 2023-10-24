8a04d5111668541ecd8ff874037caeb920280830
mukhsia <mukhsia@pdx.edu>
2023-10-24 16:14:42 -0700

    Created ApplicationDbContext class file.

---
+++ SongsWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SongsWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+    }
+}
