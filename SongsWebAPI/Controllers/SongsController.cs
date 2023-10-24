31f861b62b3e516873a8b1d839307cf18fb882e7
mukhsia <mukhsia@pdx.edu>
2023-10-24 16:28:51 -0700

    Added SongsController and finished pre-work Setup.

---
+++ SongsWebAPI/Controllers/SongsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SongsWebAPI.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SongsWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        // ApplicationDbContext and controller for querying MySQL DB
+        private readonly ApplicationDbContext _context;
+
+        public SongsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<SongsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SongsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SongsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SongsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
