5e97efd6d0632dd96059f91ae15c0c157acc1971
mukhsia <mukhsia@pdx.edu>
2023-10-24 16:23:43 -0700

    Created Model for Song and added Song DbSet into the context.

--- SongsWebAPI/Data/ApplicationDbContext.cs
+++ SongsWebAPI/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SongsWebAPI.Models;
 
 namespace SongsWebAPI.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Song> Songs { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options) 
         {
 
---
+++ SongsWebAPI/Models/Song.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SongsWebAPI.Models
+{
+    public class Song
+    {
+        [Key] public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+    }
+}
