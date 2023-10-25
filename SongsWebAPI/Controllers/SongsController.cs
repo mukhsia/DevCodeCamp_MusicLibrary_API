45e8fad992a51e8c3e9c81d277f0364b8d447067
mukhsia <mukhsia@pdx.edu>
2023-10-24 17:58:27 -0700

    Completed 'Get By Id' and 'Post' endpoints.

--- SongsWebAPI/Controllers/SongsController.cs
+++ SongsWebAPI/Controllers/SongsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SongsWebAPI.Data;
+using SongsWebAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,21 +23,32 @@ namespace SongsWebAPI.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(_context.Songs);
+            var songs = _context.Songs.ToList();
+            return Ok(songs);
         }
 
         // GET By Id: api/Songs/5
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            return Forbid();
+            var song = _context.Songs.Where(s => s.Id == id).SingleOrDefault();
+            if(song != null)
+            {
+                return Ok(song);
+            } 
+            else
+            {
+                return NotFound();
+            }
         }
 
         // POST api/Songs
         [HttpPost]
-        public IActionResult Post([FromBody] string value)
+        public IActionResult Post([FromBody] Song song)
         {
-            return Forbid();
+            _context.Songs.Add(song);
+            _context.SaveChanges();
+            return StatusCode(201, song);
         }
 
         // PUT api/Songs/5
